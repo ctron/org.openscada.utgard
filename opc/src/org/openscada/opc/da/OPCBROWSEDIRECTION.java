5ab126662bc3b64c3ece761db8f73a1ebdda2d77
Jens Reimann <jens.reimann@th4-systems.com>
2006-11-09 16:31:25 +0000

    aded OPCBROWSEDIRECTION
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/utgard/trunk@1382 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ opc/src/org/openscada/opc/da/OPCBROWSEDIRECTION.java
@@ -0,0 +1,33 @@
+package org.openscada.opc.da;
+
+public enum OPCBROWSEDIRECTION
+{
+    OPC_BROWSE_UP ( 1 ), OPC_BROWSE_DOWN ( 2 ), OPC_BROWSE_TO ( 3 ), OPC_BROWSE_UNKNOWN ( 0 );
+
+    private int _id;
+
+    private OPCBROWSEDIRECTION ( int id )
+    {
+        _id = id;
+    }
+    
+    public int id ()
+    {
+        return _id;
+    }
+    
+    public static OPCBROWSEDIRECTION fromID ( int id )
+    {
+        switch ( id )
+        {
+        case 1:
+            return OPC_BROWSE_UP;
+        case 2:
+            return OPC_BROWSE_DOWN;
+        case 3:
+            return OPC_BROWSE_TO;
+        default:
+            return OPC_BROWSE_UNKNOWN;
+        }
+    }
+}
