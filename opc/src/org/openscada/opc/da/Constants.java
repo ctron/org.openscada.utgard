8c679cd4b9b843aa1959895c31f07f6e0f974929
Jens Reimann <jens.reimann@th4-systems.com>
2006-11-10 11:25:15 +0000

    added IOPCDataCallback IID
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/utgard/trunk@1393 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- opc/src/org/openscada/opc/da/Constants.java
+++ opc/src/org/openscada/opc/da/Constants.java
@@ -9,4 +9,5 @@ public interface Constants extends org.openscada.opc.common.Constants
     public static final String IOPCItemMgt_IID = "39C13A54-011E-11D0-9675-0020AFD8ADB3";
     public static final String IOPCItemProperties_IID = "39C13A72-011E-11D0-9675-0020AFD8ADB3";
     public static final String IOPCItemIO_IID = "85C0B427-2893-4CBC-BD78-E5FC5146F08F";
+    public static final String IOPCDataCallback_IID = "39C13A70-011E-11D0-9675-0020AFD8ADB3";
 }
