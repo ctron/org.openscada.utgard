5933baa21ecc11030f14f12a98e594b6be62f6d1
Jens Reimann <jens.reimann@th4-systems.com>
2006-11-23 17:57:56 +0000

    adding unix start files
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/utgard/trunk@1502 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ opc/lib/dist/bin/preferences.sh
@@ -0,0 +1,21 @@
+set JAVA=java
+
+rem But your access data in the following variables
+rem Please not that the UUID is set to the Matrikon Simulation Server
+
+rem If the OPC server is accessable by an anonymous user
+rem You don't need to set a username and password and can
+rem leave the settings as they are.
+
+rem The test samples are currently using hard coded item names from the
+rem Matrikon Simulation server. So changing the UUID will break the test
+rem samples for the moment.
+
+set HOST=localhost
+set DOMAIN=localhost
+set USER=user
+set PASSWORD=password
+set VERSION=@@VERSION@@
+set UUID=F8582CF2-88FB-11D0-B850-00C0F0104305
+
+set TEST_CLASSPATH="../lib/j-interop.jar;../lib/jarapac.jar;../lib/jcifs-1.2.9.jar;../lib/iwombat.jar;../lib/ntlm-security.jar;../lib/ncacn_np.jar;../lib/ncacn_ip_tcp.jar;../lib/cifs-ntlm-auth.jar;../lib/log4j-1.2.13.jar;../lib/openscada-opc-dcom-%VERSION%.jar;../bin/openscada-opc-lib-%VERSION%.jar;../bin/openscada-opc-lib-test-%VERSION%.jar;."
