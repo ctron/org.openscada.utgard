5f6f0cd16a1ba63c79a1fccffc88b56b310ac751
Jens Reimann <jens.reimann@th4-systems.com>
2008-08-18 08:15:57 +0000

    update preferences
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/utgard/trunk@2841 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- opc/lib/dist/bin/preferences.bat
+++ opc/lib/dist/bin/preferences.bat
@@ -16,6 +16,11 @@ set DOMAIN=localhost
 set USER=user
 set PASSWORD=password
 set VERSION=@@VERSION@@
+
+# Matrikon OPC Simulation Server: UUID=F8582CF2-88FB-11D0-B850-00C0F0104305
+# Softing OPC Demo Server: UUID=2E565242-B238-11D3-842D-0008C779D775
+# Iconics OPC Simualtion Server: UUID=A879768A-7387-11D4-B0D8-009027242C59
+
 set UUID=F8582CF2-88FB-11D0-B850-00C0F0104305
 
-set TEST_CLASSPATH="../lib/j-interop-openscada.jar;../lib/jarapac.jar;../lib/jcifs-1.2.9.jar;../lib/iwombat.jar;../lib/ntlm-security.jar;../lib/ncacn_np.jar;../lib/ncacn_ip_tcp.jar;../lib/cifs-ntlm-auth.jar;../lib/log4j-1.2.13.jar;../lib/openscada-utils.jar;../lib/openscada-opc-dcom-%VERSION%.jar;../bin/openscada-opc-lib-%VERSION%.jar;../bin/openscada-opc-lib-test-%VERSION%.jar;."
+set TEST_CLASSPATH="../lib/j-interop-openscada.jar;../lib/j-interop.jar;../lib/jarapac.jar;../lib/jcifs-1.2.9.jar;../lib/log4j-1.2.13.jar;../lib/openscada-utils.jar;../lib/openscada-opc-dcom-%VERSION%.jar;../bin/openscada-opc-lib-%VERSION%.jar;../bin/openscada-opc-lib-test-%VERSION%.jar;."
