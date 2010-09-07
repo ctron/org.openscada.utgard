e4d20e06f01056bb141474b7b14a1b0d43d6f07b
Juergen Rose <juergen.rose@th4-systems.com>
2010-09-07 15:56:25 +0000

    fixed vendor name
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/utgard/trunk@8415 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.opc.lib.test/src/org/openscada/opc/lib/OPCTest6.java
+++ org.openscada.opc.lib.test/src/org/openscada/opc/lib/OPCTest6.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2009 TH4 SYSTEMS GmbH (http://inavare.com)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
